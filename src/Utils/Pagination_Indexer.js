f416ba3eb8005813fa00db3b827167c987af0eec
GBENGA254 <akindejuhmikel@gmail.com>
2022-09-22 22:39:54 +0100

    pagination fix

--- src/Utils/Pagination_Indexer.js
+++ src/Utils/Pagination_Indexer.js
@@ -4,7 +4,7 @@ module.exports = (pagination_index, max_results) => {
     const processed_pagination_index = none_null(pagination_index) ? 0 : parseInt(pagination_index);
     const processed_max_result = none_null(max_results) ? 20 : parseInt(max_results);
 
-    const first_index = processed_pagination_index <= 0 ? 0 : (processed_pagination_index * processed_max_result) + 1;
+    const first_index = processed_pagination_index <= 0 ? 0 : (processed_pagination_index * processed_max_result);
     const last_index = processed_pagination_index <= 0 ? processed_max_result : (processed_pagination_index + 1) * processed_max_result;
 
     return {
